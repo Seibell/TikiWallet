598ec08acd60582a19e6dbc782eb6334610e27f9
WangYuTengg <wyuteng123@gmail.com>
2023-09-06 21:02:04 +0800

    new(transactionRoutes): add in routes

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -1,5 +1,15 @@
-const express = require('express');
+const express = require("express");
+const {
+  topUpAccount,
+  withdrawFromAccount,
+  onlineTransfer,
+} = require("../controller/transactionController");
 
 const router = express.Router();
+
+// @route /transaction
+router.put("/topup", topUpAccount);
+router.put("/withdraw", withdrawFromAccount);
+router.put("/onlineTransfer", onlineTransfer);
 
 module.exports = router;
