9ad5f1f9a6132af9a2b26bf0716abb483734e77c
ryanongwx <ryanongwx@gmail.com>
2023-09-09 18:45:01 +0800

    Fix Android Stripe Bug

--- client/tiki_wallet/android/app/src/main/kotlin/com/example/tiki_wallet/MainActivity.kt
+++ client/tiki_wallet/android/app/src/main/kotlin/com/example/tiki_wallet/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.tiki_wallet
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
